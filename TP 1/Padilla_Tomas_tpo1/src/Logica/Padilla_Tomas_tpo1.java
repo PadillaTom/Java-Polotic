2fec65bf105ec598407a7e2d166b27de610c6063
Tomas Padilla <padillatomasagustin@gmail.com>
2021-04-18 12:33:51 +0200

    IGU done

---
+++ TP 1/Padilla_Tomas_tpo1/src/Logica/Padilla_Tomas_tpo1.java
@@ -0,0 +1,16 @@
+package Logica;
+
+import IGU.InterfacePrincipal;
+
+public class Padilla_Tomas_tpo1 {
+
+    public static void main(String[] args) {
+        
+        InterfacePrincipal myIGU = new InterfacePrincipal();
+        
+        myIGU.setVisible(true);
+        myIGU.setLocationRelativeTo(null);                  
+                
+    }
+    
+}
--- TP 1/Padilla_Tomas_tpo1/src/padilla_tomas_tpo1/Padilla_Tomas_tpo1.java
+++
@@ -1,9 +0,0 @@
-package padilla_tomas_tpo1;
-
-public class Padilla_Tomas_tpo1 {
-
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
-    
-}
