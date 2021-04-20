dc692dc13bace5f7553d52ddd131cb35a009be14
Tomas Padilla <padillatomasagustin@gmail.com>
2021-04-20 08:54:12 +0200

    TPO1 Done

---
+++ TP 1/Padilla_Tomas_tpo1/src/Logica/Main.java
@@ -0,0 +1,13 @@
+
+package Logica;
+
+import IGU.InterfazPrincipal;
+
+public class Main {
+   public static void main(String[] args){
+       InterfazPrincipal interfaz = new InterfazPrincipal();
+       
+       interfaz.setVisible(true);
+       interfaz.setLocationRelativeTo(null);
+   } 
+}
